d3af88a7c0a846143fc5d2d6f7d6b9dafe9e79b5
Luís Miguel <148079567+LuisMIguelFurlanettoSousa@users.noreply.github.com>
2025-02-12 09:11:05 -0300

    exercicios

--- entendendoAlgoritmos/cursoGringo/.js
+++ entendendoAlgoritmos/cursoGringo/.js
@@ -11,50 +11,71 @@ class LinkedList {
         this.head = newNode
         this.tail = this.head
         this.length = 1
+    }
+
+    push(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+    pop() {
+        if(!this.head) return undefined
+
+        let temp = this.head
+        let pre = this.head
+
+        while (temp.next){
+            pre = temp
+            temp = temp.next
+        }
+
         
     }
 
-
-   printList() {
-        let temp = this.head;
+    printList() {
+        let temp = this.head
         while (temp !== null) {
-            console.log(temp.value);
-            temp = temp.next;
+            console.log(temp.value)
+            temp = temp.next
         }
     }
 
     getHead() {
         if (this.head === null) {
-            console.log("Head: null");
+            console.log('Head: null')
         } else {
-            console.log("Head: " + this.head.value);
+            console.log('Head: ' + this.head.value)
         }
     }
 
     getTail() {
         if (this.head === null) {
-            console.log("Tail: null");
+            console.log('Tail: null')
         } else {
-            console.log("Tail: " + this.tail.value);
+            console.log('Tail: ' + this.tail.value)
         }
     }
 
     getLength() {
-        console.log("Length: " + this.length);
+        console.log('Length: ' + this.length)
     }
-    
 }
 
-
-
-let myLinkedList = new LinkedList(4);
-
-myLinkedList.getHead();
-myLinkedList.getTail();
-myLinkedList.getLength();
-console.log("\nLinked List:");
-myLinkedList.printList();
-
+let myLinkedList = new LinkedList(4)
+myLinkedList.push(7)
+myLinkedList.getHead()
+myLinkedList.getTail()
+myLinkedList.getLength()
+console.log('\nLinked List:')
+myLinkedList.printList()
 
 /*
     EXPECTED OUTPUT:
